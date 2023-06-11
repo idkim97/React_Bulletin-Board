9f853a3e3ed589fe2f50e1ab1b96c4c5b2bb9b03
idkim97 <idkim9797@naver.com>
2023-06-12 00:57:56 +0900

    Update App.js

--- bulletin/src/App.js
+++ bulletin/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import BoardDetail from './component/BoardDetail';
+import BoardList from "./component/BoardList";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BoardList />} />
+          <Route path="/detail/:id" element={<BoardDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
